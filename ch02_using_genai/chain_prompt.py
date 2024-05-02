03d3867b3f9071ae71897edd3dece2a432eced4a
Lak.Lakshmanan <lak.lakshmanan@silverlake.com>
2024-05-01 22:05:20 -0700

    Code to invoke various LLMs using DSPy

---
+++ ch02_using_genai/chain_prompt.py
@@ -0,0 +1,42 @@
+from dotenv import load_dotenv
+import os
+from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from langchain_openai import OpenAI
+from langchain_google_genai import GoogleGenerativeAI
+from langchain_groq import ChatGroq
+from langchain_anthropic import ChatAnthropic
+
+# Load key into the environment
+load_dotenv("keys.env")
+print("OPENAI_API_KEY:", os.getenv("OPENAI_API_KEY"))
+
+# let's create all 4 LLMs
+llms = [
+   # OpenAI(model_name="gpt-3.5-turbo", openai_api_key=os.getenv("OPENAI_API_KEY")),
+    GoogleGenerativeAI(model="gemini-pro", google_api_key=os.getenv("GOOGLE_API_KEY")),
+    ChatGroq(model_name="mixtral-8x7b-32768", groq_api_key=os.getenv("GROQ_API_KEY")),
+    # ChatAnthropic(model="claude-3-sonnet", api_key=os.getenv("ANTHROPIC_API_KEY"))
+]
+
+prompt = PromptTemplate.from_template("""
+Categorize the restaurant review below into one of three categories based on the sentiment of the review: Negative, Neutral, Positive and identify whether the review is discussing food, service, and/or atmosphere.
+
+**Review**:
+{review_text}
+""")
+
+output_parser = StrOutputParser()
+
+review = """
+A real gem of Cantonese cooking. Food was really hot. You can taste the "wok hei" in the food. Service was friendly and quick. Definitely coming again.
+"""
+
+for llm in llms:
+    print("\n\n\n***", llm, "***\n")
+    chain = prompt | llm | output_parser
+    response = chain.invoke({
+        "review_text": review
+    })
+    print(type(response))
+    print(response)
---
+++ ch02_using_genai/invoke_with_dspy.py
@@ -0,0 +1,55 @@
+from dotenv import load_dotenv
+import os
+import dspy
+from dspy import teleprompt
+
+# Load key into the environment
+load_dotenv("keys.env")
+
+# Control the creativity
+kwargs = {
+    "temperature": 0.1,
+    "top_p": 1.0,
+    # "top_k": 1,
+    # "num_generations": 1
+}
+
+# let's create all 4 LLMs
+llms = [
+    # dspy.OpenAI(model="gpt-3.5-turbo", api_key=os.getenv("OPENAI_API_KEY"), **kwargs),
+    dspy.Google(model="models/gemini-1.0-pro", api_key=os.getenv("GOOGLE_API_KEY"), **kwargs),
+    dspy.GROQ(model="mixtral-8x7b-32768", api_key=os.getenv("GROQ_API_KEY"), **kwargs),
+    # dspy.Claude(model="claude-3-sonnet", api_key=os.getenv("ANTHROPIC_API_KEY"), **kwargs)
+]
+
+
+class ReviewClassifier(dspy.Module):
+    """
+    Given a review, specifies the sentiment and identifies the topics discussed.
+    """
+    def __init__(self):
+        super().__init__()
+        self.context = """
+        Categorize the restaurant review below into one of three categories based on the sentiment of the review: Negative, Neutral, Positive and identify whether the review is discussing food, service, and/or atmosphere.
+        """
+        self.prog = dspy.Predict("context, review -> sentiment, topics, explanation")
+
+    def forward(self, review):
+        answer = self.prog(context=self.context, review=review)
+        if "Sentiment: " in answer.sentiment:
+            start = answer.sentiment.rindex("Sentiment: ") + len("Sentiment: ")
+            answer.sentiment = answer.sentiment[start:]
+        answer.topics = [x.strip() for x in answer.topics.split(",")]
+        return answer
+
+
+for llm in llms:
+    with dspy.settings.context(lm=llm):
+        print(llm.__class__.__name__)
+        classifier = ReviewClassifier()
+        response = classifier("A real gem of Cantonese cooking. Food was really hot. You can taste the 'wok hei' in the food. Service was friendly and quick. Definitely coming again.")
+        print("**Sentiment**: ", response.sentiment)
+        print("**Topics**: ", response.topics)
+        print("**Explanation**: ", response.explanation)
+
+
